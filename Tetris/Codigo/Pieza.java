6f503f17fd279a75a6545c3f992967695fdbfb0d
Derek <derekliangzheng@gmail.com>
2025-11-04 22:23:38 -0600

    Creación de pieza: forma y color

--- Tetris/Codigo/Pieza.java
+++ Tetris/Codigo/Pieza.java
@@ -1,23 +1,105 @@
 /*
- * Al menos las 4 FORMAS del tetris clásico
- * 
- * El COLOR es ALEATORIO, cada bloque de la pieza tiene un color aleatorio (puede ser diferente)
- * 
- * Los COLORES dan PUNTOS dependiendo de cómo están posicionados según su FRECUENCIA en el ÁRBOL AUTOBALANCEADO
- */
+* Reglas:
+* 
+* - mínimo 4 formas del tetris clásico
+* - cada pieza tiene diferentes colores
+* - los colores son aleatorios
+* - el puntaje recibido al romper una fila se determina en base a los colores y su frecuencia en el árbol autobalanceado
+* - la pieza puede rotar: A (contra reloj), D (sentido reloj)
+* - otros controles: S (bajar la pieza unas cuantas casillas), ENTER (colocar la pieza)
+* - la pieza no cae automáticamente
+*/
+
 public class Pieza {
     private int[][] forma;
-    private int color; // color = número, facilita el print
+    private String[] colores;
 
-    public Pieza(int[][] forma, int color) {
-        this.forma = forma;
-        this.color = color;
+    public Pieza() {
+        definirForma();
+        asignarColores();
     }
 
-/*
- * Método rotar: izquierda, derecha y abajo en sentido reloj
- * Controles: WASD y enter (bajar la pieza de una)
- * La pieza no baja a menos que se pulse S (abajo)
- * W no tiene función en este programa.
- */
+    // Genera la forma de la pieza
+    // 1: bloque
+    // 0: vacío
+    private void definirForma() {
+        switch (numeroAleatorio(5)) { // 5 formas
+            // Forma línea
+            case 1:
+                forma = new int[][] {
+                    {1},
+                    {1},
+                    {1},
+                    {1}
+                };
+                break;
+            
+            // Forma cuadrado
+            case 2:
+                forma = new int[][] {
+                    {1, 1},
+                    {1, 1}
+                };
+                break;
+
+            // Forma L
+            case 3:
+                forma = new int[][] {
+                    {1, 0},
+                    {1, 0},
+                    {1, 1}
+                };
+                break;
+
+            // Forma T
+            case 4:
+                forma = new int[][] {
+                    {0, 1, 0},
+                    {1, 1, 1}
+                };
+                break;
+            
+            // Forma zigzag
+            case 5:
+                forma = new int[][] {
+                    {1, 1, 0},
+                    {0, 1, 1}
+                };
+                break;
+        }
+    }
+
+    // Genera una lista de colores para emparejar con la forma
+    private void asignarColores() {
+        colores = new String[4]; // Ya que todas las piezas están compuestas de 4 bloques
+        for (int i = 0; i < colores.length; i++) {
+            colores[i] = colorAleatorio();
+        }
+    }
+
+    // Genera un número aleatorio hasta el límite (incluyendo al límite)
+    private int numeroAleatorio(int limite) {
+        return (int) (Math.random() * limite) + 1;
+    }
+
+    // Genera un color aleatorio para identificar luego
+    private String colorAleatorio() {
+        switch (numeroAleatorio(5)) {
+            case 1: return "RED";
+            case 2: return "GREEN";
+            case 3: return "YELLOW";
+            case 4: return "BLUE";
+            case 5: return "CYAN";
+            default: return "ERROR";
+        }
+    }
+
+    // Getters
+
+    public int[][] getForma() {
+        return forma;
+    }
+    public String[] getColores() {
+        return colores;
+    }
 }
